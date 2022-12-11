9d27cc957db8efd082815cad5dea50a999fbdae3
Konstantin <zaikinke@yandex.ru>
2022-12-11 15:26:21 +0500

    Add new validators

--- validators.py
+++ validators.py
@@ -1,7 +1,8 @@
 from ipaddress import ip_address
+from enums import Error
 
 
-def is_ip_address(value: str) -> bool:
+def validate_ip_address(value: str) -> bool:
     try:
         ip_address(value)
     except ValueError:
@@ -10,5 +11,23 @@ def is_ip_address(value: str) -> bool:
     return True
 
 
-def is_port(value: int) -> bool:
+def validate_port(value: int) -> bool:
+    if value is None:
+        return True
     return 0 <= value <= 65535
+
+
+def validate_timeout(value: float) -> bool:
+    return value > 0
+
+
+def validate_args(ip_address: str, port: int, timeout: float) -> Error | None:
+    if not validate_ip_address(ip_address):
+        print("Invalid IP address")
+        return Error.INVALID_IP
+    elif not validate_port(port):
+        print("Invalid port")
+        return Error.INVALID_PORT
+    elif not validate_timeout(timeout):
+        print("Invalid timeout")
+        return Error.NEGATIVE_ARGUMENT
